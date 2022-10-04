ecc5d3c42c483426f9977f08197d702364e74546
subodhNagalwade <subodhnagalwade@gmail.com>
2022-10-04 16:45:02 +0530

    [SUBODH]Create a contact to adress book

---
+++ AddressBook/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace AddressBookProj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressBook addressBook = new AddressBook();
+
+            addressBook.AddContact("Subodh", "Nagalwade", "Age-25 ", "Lakhani", "Maharashtra", 441804, 9422136430, "subodhnagalwade@gmail.com");
+
+
+            addressBook.AddContact("Rutvik", "Dhande", "Age-22", "lakhani", "maharashtra", 441804, 9503913851, "Rutvikdhande@gmail.com");
+
+            addressBook.ViewContact();
+        }
+    }
+}
